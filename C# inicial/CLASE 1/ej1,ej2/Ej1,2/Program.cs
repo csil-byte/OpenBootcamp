e464b6161327afc3031a911af8b9b1a255afffcd
csil-byte <cgscsilva@gmail.com>
2022-08-10 15:29:05 -0300

    commit inicial

---
+++ C# inicial/CLASE 1/ej1,ej2/Ej1,2/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         Console.WriteLine("Hola! Favor escribir tu nombre");
+        string nombre = Console.ReadLine();
+        Console.WriteLine("Hola, " + nombre); 
+    }
+}
